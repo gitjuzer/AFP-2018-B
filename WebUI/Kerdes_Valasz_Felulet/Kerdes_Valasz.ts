629051fb333033567e1f93449e21dcda81a8e6e5
kaszasneni <43805378+kaszasneni@users.noreply.github.com>
2018-11-26 21:06:02 +0100

    Kérdés-Válasz felület logikájának továbbfejlesztése.

---
+++ WebUI/Kerdes_Valasz_Felulet/Kerdes_Valasz.ts
@@ -0,0 +1,104 @@
+let countdownNumberEl = document.getElementsByClassName('countdown-number');
+let animation = document.getElementsByClassName('run-animation');
+let countdown = 30;
+let Timer;
+let helyes;
+let Pontszam = 0;
+let QuizContainer = document.getElementsByClassName('answer');
+let questionOrder = [];
+questionOrder.length = 10;
+let questionCount = 0;
+let questionAsked = 0;
+
+/*
+window.onload = function() {
+  while (questionCount < questionOrder.length) {
+    hideAllExcept(questionOrder[questionAsked]);
+    questionCount++;
+  }
+};
+function hideAllExcept(question) {
+  $('div.ques').hide();
+  $('#ques' + question).show();
+}*/
+setInterval(function() {
+  if (countdown = -- countdown < 0 ? 30 : countdown) {
+    countdownNumberEl[0].textContent = countdown.toString();
+    countdownNumberEl[1].textContent = countdown.toString();
+  }
+  else if (countdown === 0) {
+    TimeRunsOut();
+    countdownReset();
+  }
+}, 1000);
+
+
+
+function TimeRunsOut() {
+    clearInterval(Timer);
+
+    if (Pontszam === 0) {
+      Pontszam = 0;
+      questionAsked++;
+    }
+    else if (Pontszam > 1000) {
+      Pontszam -= 300;
+      questionAsked++;
+    }
+    alert('Lejárt az időd! Kattints a továbblépéshez!');
+    countdownReset();
+    Answer();
+}
+
+function GoodAnswer() {
+  clearInterval(Timer);
+  if (countdown > 20) {
+    Pontszam += 600;
+    questionAsked++;
+  }
+  else if (countdown > 10) {
+    Pontszam += 300;
+    questionAsked++;
+  }
+  else if (countdown > 0) {
+    Pontszam += 150;
+    questionAsked++;
+  }
+  alert('Helyes válasz! Kattints a továbblépéshez!' + questionAsked + ':' + Pontszam);
+  countdownReset();
+  Answer();
+}
+
+function BadAnswer() {
+  clearInterval(Timer);
+  if (Pontszam === 0) {
+    Pontszam = 0;
+    questionAsked++;
+  }
+  else if (Pontszam > 1000) {
+    Pontszam -= 500;
+    questionAsked++;
+  }
+  alert('Helytelen válasz! Kattints a továbblépéshez!');
+  countdownReset();
+  Answer();
+}
+
+function Answer() {
+  if (questionAsked === questionOrder.length) {
+    alert('Végeztél a kvízzel!' + questionAsked + ':' + questionOrder.length);
+  }
+  {
+    // hideAllExcept(questionOrder[questionAsked]);
+  }
+}
+
+
+
+function countdownPause() {
+  clearTimeout(countdown);
+}
+function countdownReset() {
+  countdownPause();
+  countdown = 30;
+}
