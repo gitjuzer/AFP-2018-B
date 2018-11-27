08f456054828fb76e69f0811cc351d94339cd6bc
Bálint Bogáti <balint1994@gmail.com>
2018-11-27 16:14:04 +0100

    Android - Regisztrációs felület mezőinek validálása

--- Android/app/src/main/java/hu/tanuloapp/afp/MainActivity.java
+++ Android/app/src/main/java/hu/tanuloapp/afp/MainActivity.java
@@ -1,11 +1,13 @@
 package hu.tanuloapp.afp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.common.hash.Hashing;
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button login = findViewById(R.id.login);
+        TextView newacc = findViewById(R.id.registration);
         email = findViewById(R.id.email);
         pass = findViewById(R.id.pass);
         login.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
                 Login();
             }
         });
+        newacc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoToRegister();
+            }
+        });
+    }
+
+    public void GoToRegister(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        finish();
+        startActivity(intent);
     }
 
     private void Login() {
--- Android/app/src/main/java/hu/tanuloapp/afp/RegisterActivity.java
+++ Android/app/src/main/java/hu/tanuloapp/afp/RegisterActivity.java
@@ -1,9 +1,16 @@
 package hu.tanuloapp.afp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.StandardCharsets;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -19,5 +26,38 @@ public class RegisterActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         pass = findViewById(R.id.pass);
         confirmpass = findViewById(R.id.confirmpass);
+        registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Register();
+            }
+        });
+        backtologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BackToLogin();
+            }
+        });
+    }
+
+    public void BackToLogin(){
+        Intent intent = new Intent(this,MainActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+    private void Register(){
+        String usernameValue = username.getText().toString();
+        String emailValue = email.getText().toString();
+        String passValue = pass.getText().toString();
+        String confirmpassValue = confirmpass.getText().toString();
+        if ("".equals(usernameValue) || "".equals(emailValue) || "".equals(passValue) || "".equals(confirmpassValue))
+            Toast.makeText(this, R.string.fill_all_field, Toast.LENGTH_LONG).show();
+        else if(!passValue.equals(confirmpassValue)){
+            Toast.makeText(this, R.string.pass_dont_match, Toast.LENGTH_LONG).show();
+        }
+        else {
+            passValue = Hashing.sha256().hashString(passValue, StandardCharsets.UTF_8).toString();
+        }
     }
 }
