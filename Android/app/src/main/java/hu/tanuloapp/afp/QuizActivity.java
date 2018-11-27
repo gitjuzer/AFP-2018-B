7dbfa313c2442746c29f3a9ebd992604f78512b2
AndorSebo <andorsebo@gmail.com>
2018-11-27 23:49:44 +0100

    Quiz UI implementálása kódba

---
+++ Android/app/src/main/java/hu/tanuloapp/afp/QuizActivity.java
@@ -0,0 +1,13 @@
+package hu.tanuloapp.afp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class QuizActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+
+}
