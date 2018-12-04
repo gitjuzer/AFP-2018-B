676d77ea0a5d3a8f797df36ad5ecaf47fdaa3c6e
kaszasneni <43805378+kaszasneni@users.noreply.github.com>
2018-12-05 00:11:22 +0100

    CSS jellemzők Angularba migrálása

--- WebUI/Kerdes_Valasz_Felulet/src/app/app.module.ts
+++ WebUI/Kerdes_Valasz_Felulet/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QuizContainerComponent } from './quiz-container/quiz-container.component';
+import { TableComponent } from './table/table.component';
+import { ButtonComponent } from './button/button.component';
+import { CircleComponent } from './circle/circle.component';
+import { Table2Component } from './table2/table2.component';
+import { SvgComponent } from './svg/svg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizContainerComponent
+    QuizContainerComponent,
+    TableComponent,
+    ButtonComponent,
+    CircleComponent,
+    Table2Component,
+    SvgComponent
   ],
   imports: [
     BrowserModule
