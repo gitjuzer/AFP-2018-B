5f9ee90d3cf13ec9b8b41fc7a9e9fbb30fe17a7b
kaszasneni <43805378+kaszasneni@users.noreply.github.com>
2018-12-05 00:17:48 +0100

    CSS tulajdonságok Angularba migrálás javítás: új fájlokat nem vette be

---
+++ WebUI/Kerdes_Valasz_Felulet/src/app/circle/circle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-circle',
+  templateUrl: './circle.component.html',
+  styleUrls: ['./circle.component.css']
+})
+export class CircleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ WebUI/Kerdes_Valasz_Felulet/src/app/table2/table2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table2',
+  templateUrl: './table2.component.html',
+  styleUrls: ['./table2.component.css']
+})
+export class Table2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
