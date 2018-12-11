a7a9c4be6ee1b596fecb10a50fb23a22ec36378a
Bálint Bogáti <balint1994@gmail.com>
2018-12-11 11:50:17 +0100

    Menü megnyitása/bezárása a toolbaron lévő gombbal

--- Android/app/src/main/java/hu/tanuloapp/afp/MainActivity.java
+++ Android/app/src/main/java/hu/tanuloapp/afp/MainActivity.java
@@ -1,13 +1,21 @@
 package hu.tanuloapp.afp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.MenuItem;
+import android.support.v4.view.GravityCompat;
 
 import com.google.common.hash.Hashing;
 
@@ -23,11 +31,18 @@ public class MainActivity extends AppCompatActivity {
 
     EditText email, pass;
     User user;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Button login = findViewById(R.id.login);
         TextView newacc = findViewById(R.id.registration);
         Intent intent = getIntent();
@@ -53,6 +68,14 @@ public class MainActivity extends AppCompatActivity {
                 GoToActivity(RegisterActivity.class);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void GoToActivity(Class toActivity) {
