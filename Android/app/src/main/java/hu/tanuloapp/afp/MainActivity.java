1a90ca2156baad540e33986f2a32cb7b601e3fdd
Bálint Bogáti <balint1994@gmail.com>
2018-12-11 17:20:20 +0100

    Menü implementálása a ProfileActivityben

--- Android/app/src/main/java/hu/tanuloapp/afp/MainActivity.java
+++ Android/app/src/main/java/hu/tanuloapp/afp/MainActivity.java
@@ -2,6 +2,9 @@ package hu.tanuloapp.afp;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -47,6 +50,26 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                menuItem.setChecked(true);
+                if(id == R.id.profile){
+                    GoToActivity(ProfilActivity.class);
+                }else if(id == R.id.quiz){
+                    GoToActivity(QuizActivity.class);
+                }else if(id == R.id.logout){
+
+                }
+
+                DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
         Button login = findViewById(R.id.login);
         TextView newacc = findViewById(R.id.registration);
         Intent intent = getIntent();
