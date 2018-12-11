58184c099e036ded95384ffeb9df1a3d0dc2ab43
Bálint Bogáti <balint1994@gmail.com>
2018-12-11 17:21:13 +0100

    Menü implementálása a QuizActivityben

--- Android/app/src/main/java/hu/tanuloapp/afp/QuizActivity.java
+++ Android/app/src/main/java/hu/tanuloapp/afp/QuizActivity.java
@@ -1,15 +1,26 @@
 package hu.tanuloapp.afp;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class QuizActivity extends AppCompatActivity {
     long timeLong;
+    private ActionBarDrawerToggle mToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,37 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         StartTimer();
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        DrawerLayout mDrawerLayout = findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                menuItem.setChecked(true);
+                if(id == R.id.profile){
+                    GoToActivity(ProfilActivity.class);
+                }else if(id == R.id.quiz){
+                    GoToActivity(QuizActivity.class);
+                }else if(id == R.id.logout){
+
+                }
+
+                DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 
     private void StartTimer() {
@@ -38,6 +80,12 @@ public class QuizActivity extends AppCompatActivity {
     private void StopTimer() {
         Toast.makeText(this, "Következő kérdés", Toast.LENGTH_LONG).show();
         StartTimer();
+    }
+
+    public void GoToActivity(Class toActivity) {
+        Intent intent = new Intent(this, toActivity);
+        finish();
+        startActivity(intent);
     }
 
 }
